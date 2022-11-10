4368b85dca61fef1b91db308d1c3768a4117acf6
Morgan <redfieldm@gmail.com>
2022-11-10 15:16:56 -0500

    add basic handlers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use axum::{
     routing::get,
     Router,
+    response::{Html, IntoResponse},
+    http,
+    handler::Handler,
 };
 
 #[tokio::main]
@@ -9,12 +12,39 @@ async fn main() {
 
     // build application with route
     let app = Router::new()
-        .route("/", get(|| async { "Hello, world!" }))
-        .route("/blog", get(|| async { "Hello blogger!" }));
+        .route("/", get(home))
+        .route("/blog", get(blog)) // todo: post-level slug
+        .route("/contact", get(contact))
+        .fallback(fallback.into_service());
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+pub async fn fallback(uri: http::Uri) -> impl IntoResponse {
+    (http::StatusCode::NOT_FOUND, format!("404: No route {}", uri))
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("expect tokio signal ctl-c");
+    println!("signal shutdown");
+}
+
+
+async fn home() -> Html<String> {
+    Html(format!("<p>{}</p>", "Hello world"))
+}
+
+async fn blog() -> Html<String> {
+    Html(format!("<p>{}</p>", "Blog Incoming"))
+}
+
+async fn contact() -> Html<String> {
+    Html(format!("<p>{}</p>", "find me here"))
 }
