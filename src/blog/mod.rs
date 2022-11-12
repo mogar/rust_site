eadd9f1cfc1ec40badd9d842e693824f8f2ece63
Morgan <redfieldm@gmail.com>
2022-11-11 22:17:19 -0500

    minor comment cleanup

--- src/blog/mod.rs
+++ src/blog/mod.rs
@@ -1,10 +1,14 @@
 
-use markdown;
-
 /// Generate an index of blog posts
 /// This will eventually iterate through markdown files in a
 /// directory and populate an index at runtime
 pub fn index() -> String {
+    // TODO: get list blog posts in directory
+
+    // for file in posts
+    // generate summary and title
+    // images?
+
     markdown::to_html("# Blog _Incoming_")
 }
 
@@ -12,6 +16,14 @@ pub fn index() -> String {
 /// This will eventually parse and render a blog
 /// post from a given markdown file.
 pub fn post(slug: String) -> String {
+    // TODO: convert slug to filename
+
+    // TODO: read entire file into string md
+
+    // TODO: extract metadata (SEO stuff??)
+
+    // TODO: format SEO stuff separately
+
     let md = format!("# post name\n\npost id {}", slug);
     markdown::to_html(&md)
 }
