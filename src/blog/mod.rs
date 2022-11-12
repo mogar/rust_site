e1a995e65330e2d709baa53d75e027006bef5535
Morgan <redfieldm@gmail.com>
2022-11-11 21:35:54 -0500

    start using markdown parser for blog

--- src/blog/mod.rs
+++ src/blog/mod.rs
@@ -1,15 +1,17 @@
 
+use markdown;
+
 /// Generate an index of blog posts
 /// This will eventually iterate through markdown files in a
 /// directory and populate an index at runtime
 pub fn index() -> String {
-    "<h1>Blog Incoming</h1>".to_string()
+    markdown::to_html("# Blog _Incoming_")
 }
 
 /// Render and display a single blog post
 /// This will eventually parse and render a blog
 /// post from a given markdown file.
 pub fn post(slug: String) -> String {
-    format!("<h1>post name</h1>
-    <p>post id {}</p>", slug)
+    let md = format!("# post name\n\npost id {}", slug);
+    markdown::to_html(&md)
 }
